f913f9786603a77d6a6d3e09fe46cb2bd36b847a
Sebastian Beleño <sebasbeleno15@gmail.com>
2024-05-17 01:13:57 -0500

    navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,11 +11,9 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   interface TokenInfoType {
